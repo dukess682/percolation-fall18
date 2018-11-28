638049b69b0a4faf58323c3027d81acb68b52e2e
Samhitha Sunkara <ss682@duke.edu>
2018-11-28 13:54:53 -0500

    edited

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -6,11 +6,31 @@ public class PercolationDFSFast extends PercolationDFS {
 	
 	@Override
 	protected void updateOnOpen (int row, int col){
-		if (row == 0 || inBounds(row-1,col) && isFull(row-1,col) || inBounds(row+1,col) && isFull(row+1,col) || 
-				inBounds(row,col-1)&& isFull(row,col-1) || inBounds(row,col+1)&&isFull(row,col+1)) {
+//		if (row == 0 || inBounds(row-1,col) && isFull(row-1,col) || inBounds(row+1,col) && isFull(row+1,col) || 
+//				inBounds(row,col-1)&& isFull(row,col-1) || inBounds(row,col+1)&&isFull(row,col+1)) {
+//			dfs(row,col);
+//		}
+		if (row ==0) {
 			dfs(row,col);
+			return;
 		}
-		return;
+		else if(inBounds(row-1,col) && isFull(row-1,col)) {
+			dfs(row,col);
+			return;
+		}
+		else if (inBounds(row+1,col) && isFull(row+1,col)) {
+			dfs(row,col);
+			return;
+		}
+		else if (inBounds(row,col-1)&& isFull(row,col-1)){
+			dfs(row,col);
+			return;
+		}
+		else if (inBounds(row,col+1)&&isFull(row,col+1)) {
+			dfs(row,col);
+			return;
+		}
+		
 	}
 }
 
